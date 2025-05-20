44d4999310cca53f61636ee14466f6bdc97c2338
Valentin <none>
2025-05-20 21:50:51 +0800

    Добавлен запрос Users

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -10,6 +10,7 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return User::all();
+        $users = User::all();
+        return response()->json($users);
     }
 }
